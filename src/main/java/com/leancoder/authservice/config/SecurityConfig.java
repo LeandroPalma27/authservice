82c4ffb15c168e3e7fd637c2dac9820c52dce75d
Leandro <psychomusicbox@gmail.com>
2024-05-27 16:10:44 -0500

    inicio de proyecto

---
+++ src/main/java/com/leancoder/authservice/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.leancoder.authservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnabledWebSecurity
+public class SecurityConfig {
+    
+}
